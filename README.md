33db749315756f23e44007052f4eeac0867eacdb
dynamyc <balint.sebestyen2004@gmail.com>
2022-05-10 06:00:55 +0200

    Teeny-tiiiny touchups, also README planned

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Moodleee
- Discord Bot for gathering information about tasks you need to give in
+ A Discord bot used for gathering your due assignments easily from Discord.
+
+## Currently it can:
+- Register multiple users
+- Gather your assingments
+- Make you cry how bad the Moodle REST API is
+
+## Planned:
+- Gather assignments you missed (and haven't given in anything for)
+- Figure out what the hell most of the API does
--- commands/register.js
+++ commands/register.js
@@ -32,50 +32,48 @@ module.exports = {
                 .setName('disconnect')
                 .setDescription('Disconnect your Moodle account from your Discord account.')),
 	async execute(interaction) {
-        if(interaction.options.getSubcommand() === 'connect'){
-            await interaction.deferReply({ ephemeral: true });
-            moodle.init({
-                wwwroot: config.moodle.url,
-                username: interaction.options.getString('username'),
-                password: interaction.options.getString('password')
-              }).then(function(client) {
-                if(contains(config.moodle.tokens, "discordid", interaction.member.id)){
-                  //console.log("Token already exists for this discord user");
-                  config.moodle.tokens[config.moodle.tokens.findIndex(x => x.discordid === interaction.member.id)].token = client.token;
-                }
-                else{
-                  //console.log("Token does not exist for this discord user");
-                  config.moodle.tokens.push({"discordid": interaction.member.id, "token": client.token});
-                }
-                fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
-                //console.log('Token written to config.json successfully!');
-                successEmbed
-                  .setDescription(`You are now connected to Moodle!`)
-                return interaction.editReply({embeds: [successEmbed], ephemeral: true});
-              })
-              .catch(function(err) {
-                //console.log("Failed to log in: " + err);
-                errorEmbed
-                  .setDescription("Failed to connect your Moodle account!")
-                  .setFooter({text: `Please try again later. ${err}`});
-                return interaction.editReply({embeds: [errorEmbed], ephemeral: true});
-              });
-                
-        }
-        else if(interaction.options.getSubcommand() === 'disconnect'){
-            await interaction.deferReply({ ephemeral: true });
+    await interaction.deferReply({ ephemeral: true });
+    if(interaction.options.getSubcommand() === 'connect'){
+        moodle.init({
+            wwwroot: config.moodle.url,
+            username: interaction.options.getString('username'),
+            password: interaction.options.getString('password')
+          }).then(function(client) {
             if(contains(config.moodle.tokens, "discordid", interaction.member.id)){
-                config.moodle.tokens.splice(config.moodle.tokens.findIndex(x => x.discordid === interaction.member.id), 1);
-                fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
-                successEmbed
-                  .setDescription(`You are now disconnected from Moodle!`)
-                return interaction.editReply({embeds: [successEmbed], ephemeral: true});
-            }else{
-                errorEmbed
-                  .setDescription("You are not connected to Moodle!")
-                  .setFooter({text: `Connect your account first.`});
-                await interaction.editReply({embeds: [errorEmbed], ephemeral: true});
+              //console.log("Token already exists for this discord user");
+              config.moodle.tokens[config.moodle.tokens.findIndex(x => x.discordid === interaction.member.id)].token = client.token;
             }
-        };
+            else{
+              //console.log("Token does not exist for this discord user");
+              config.moodle.tokens.push({"discordid": interaction.member.id, "token": client.token});
+            }
+            fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
+            //console.log('Token written to config.json successfully!');
+            successEmbed
+              .setDescription(`You are now connected to Moodle!`)
+            return interaction.editReply({embeds: [successEmbed], ephemeral: true});
+          })
+          .catch(function(err) {
+            //console.log("Failed to log in: " + err);
+            errorEmbed
+              .setDescription("Failed to connect your Moodle account!")
+              .setFooter({text: `Please try again later. ${err}`});
+            return interaction.editReply({embeds: [errorEmbed], ephemeral: true});
+          });
+      }
+    else if(interaction.options.getSubcommand() === 'disconnect'){
+      if(contains(config.moodle.tokens, "discordid", interaction.member.id)){
+          config.moodle.tokens.splice(config.moodle.tokens.findIndex(x => x.discordid === interaction.member.id), 1);
+          fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
+          successEmbed
+            .setDescription(`You are now disconnected from Moodle!`)
+          return interaction.editReply({embeds: [successEmbed], ephemeral: true});
+      }else{
+          errorEmbed
+            .setDescription("You are not connected to Moodle!")
+            .setFooter({text: `Connect your account first.`});
+          await interaction.editReply({embeds: [errorEmbed], ephemeral: true});
+      }
+    };
 	},
 };
